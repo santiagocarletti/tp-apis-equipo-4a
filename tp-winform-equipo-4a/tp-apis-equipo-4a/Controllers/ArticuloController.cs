7fc0ffb02fe5e0c798a30b0516ee61c202120dc2
Nicolas Fernandezn <fernandeznn408@gmail.com>
2025-10-11 22:28:22 -0300

    Validacion en POST

--- tp-winform-equipo-4a/tp-apis-equipo-4a/Controllers/ArticuloController.cs
+++ tp-winform-equipo-4a/tp-apis-equipo-4a/Controllers/ArticuloController.cs
@@ -27,19 +27,51 @@ namespace tp_apis_equipo_4a.Controllers
         }
 
         // POST: api/Articulo
-        public void Post([FromBody]ArticuloDto articulo)
+        public HttpResponseMessage Post([FromBody]ArticuloDto articulo)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo nuevo = new Articulo();
-            nuevo.Codigo = articulo.Codigo;
-            nuevo.Nombre = articulo.Nombre;
-            nuevo.Descripcion = articulo.Descripcion;
-            nuevo.marca = new Marca { Id = articulo.IdMarca };
-            nuevo.IdCategoria = new Categoria { Id = articulo.IdCategoria };
-            nuevo.Precio = articulo.Precio;
-            nuevo.Imagen = articulo.Imagen;
+            //ArticuloNegocio negocio = new ArticuloNegocio();
+            //Articulo nuevo = new Articulo();
+            //nuevo.Codigo = articulo.Codigo;
+            //nuevo.Nombre = articulo.Nombre;
+            //nuevo.Descripcion = articulo.Descripcion;
+            //nuevo.marca = new Marca { Id = articulo.IdMarca };
+            //nuevo.IdCategoria = new Categoria { Id = articulo.IdCategoria };
+            //nuevo.Precio = articulo.Precio;
+            //nuevo.Imagen = articulo.Imagen;
 
-            negocio.agregar(nuevo);
+            var negocio = new ArticuloNegocio();
+            var marcaNegocio = new MarcaNegocio();
+            var categoriaNegocio = new CategoriaNegocio();
+
+            Marca marca = marcaNegocio.listar().Find(x => x.Id == articulo.IdMarca);
+            Categoria categoria = categoriaNegocio.listar().Find(x => x.Id == articulo.IdCategoria);
+
+            if (marca == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La marca no existe.");
+
+            if (categoria == null)
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "La categoría no existe.");
+
+            var nuevo = new Articulo
+            {
+                Codigo = articulo.Codigo,
+                Nombre = articulo.Nombre,
+                Descripcion = articulo.Descripcion,
+                marca = marca,
+                IdCategoria = categoria,
+                Precio = articulo.Precio,
+                Imagen = articulo.Imagen
+            };
+
+            try
+            {
+                negocio.agregar(nuevo);
+                return Request.CreateResponse(HttpStatusCode.OK, "Artículo agregado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error al agregar el artículo");
+            }
         }
 
         // PUT: api/Articulo/5
